86acdd2baba98e9c7fb4e100a7754e796dd900ad
azhad123 <mazhad522@gmail.com>
2024-07-18 21:55:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7043144074:AAHgSU5EwRsitJJ6Ggw5OjDDkQvanc9mHy8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("DARKxBAN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5290136861")
 
 
 
